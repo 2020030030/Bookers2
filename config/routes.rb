cfeada10dad19c6bcc4d3067cbabf481105075f0
2020030030 <nagomicunshang@gmail.com>
2021-09-30 04:51:05 +0000

    課題４

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to:'home#top'
   get 'home/about' => 'home#about'
-  resources :users
 
   resources :books do
   resource :favorites, only: [:create, :destroy]
   resources :book_comments, only: [:create, :destroy]
+  end
+
+  resources :users do
+  resource :relationships, only: [:create, :destroy]
+  	get 'followings' => 'relationships#followings', as: 'followings'
+  	get 'followers' => 'relationships#followers', as: 'followers'
   end
 
 end
