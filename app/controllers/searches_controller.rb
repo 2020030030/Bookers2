8eb8264610889a3f5d14c8680762dc1aabbcfdcc
2020030030 <nagomicunshang@gmail.com>
2021-10-02 05:35:38 +0000

    応用課題５

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,11 +1,10 @@
 class SearchesController < ApplicationController
   before_action :authenticate_user!
 
-  def searche
+  def search
     	@model = params[:model]
 		@content = params[:content]
 		@method = params[:method]
-		binding.pry
 		if @model == 'user'
 			@records = User.search_for(@content, @method)
 		else
